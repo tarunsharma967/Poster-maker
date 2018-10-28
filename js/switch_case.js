03a2748f644f49566d2c2b3e671af57062c6f05b
Shubham Gupta <shubhamgupta7440@gmail.com>
2018-10-28 20:36:33 +0530

    Create switch_case.js

---
+++ js/switch_case.js
@@ -0,0 +1,18 @@
+var ch=2;
+switch(ch)
+{
+  case 1:
+    alert("Hello World!");
+    break;
+    
+  case 2:
+    alert("Hello Hacktoberfest, its time of contribution.");
+    break;
+    
+  case 3:
+    document.write("<h2>Example of sswitch case</h2>");
+    break;
+    
+  default:
+    document.write("wrong choice");
+}
